79496fd5c89ba87cd68469dffcc95e73f0b13717
PANCHO7532 <32972267+PANCHO7532@users.noreply.github.com>
2021-05-20 21:09:21 +0000

    javascript stuff

---
+++ javascript-stuff/xorSearch.js
@@ -0,0 +1,25 @@
+/*
+* /=============================================\
+* | XOR Decoder                                 |
+* | Copyright PANCHO7532 - P7COMunications LLC  |
+* \=============================================/
+* This utility calculates XOR keys/values by doing a comparison of values
+* Most repeated sequence is probably the correct XOR key/sequence required for decrypt the file.
+*/
+const fs = require('fs');
+const file1 = fs.readFileSync("xorcrypted.dat").toString(); //original file
+const file2 = fs.readFileSync("xordecoded.dat").toString(); //decoded file
+var arr = [];
+for(let c = 0; c < file2.length; c++) {
+    for(let d = 0;; d++) {
+        if((file1.charCodeAt(c) ^ d) == file2.charCodeAt(c)) {
+            arr.push(d);
+            break;
+        }
+    }
+}
+console.log(arr); //array of values
+for(let c = 0; c < arr.length; c++) {
+    process.stdout.write(String.fromCharCode(arr[c]) + "");
+}
+console.log();
